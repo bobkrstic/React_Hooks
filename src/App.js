9c08f6da870e5758db7086561118e38c8cbde422
bobkrstic <slobodankrsticnis@yahoo.com>
2019-10-29 17:31:05 -0700

    Functionality set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add todo"
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Lunch Meeting",
+      isCompleted: false
+    },
+    {
+      text: "Study react.js",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
